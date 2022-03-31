62a7d657106f5f47fbe2e696d05037038d45f850
Peter Hopka <peter.hopka1988@gmail.com>
2022-03-31 20:44:14 +0200

    move to raylib

--- Simfarm/Simfarm/src/Renderer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Renderer.h"
-
-void Renderer::Render(const VertexBuffer* vb, const IndexBuffer* ib, const Shader* shader) const
-{
-	vb->Bind();
-	ib->Bind();
-
-	glDrawElements(GL_TRIANGLES, ib->GetCount(), GL_UNSIGNED_INT, 0);
-}
